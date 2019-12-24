774a0cdf4300bd1ed5473569f0710d2eb71d6258
Connor Lee <45024235+connorleee@users.noreply.github.com>
2019-12-23 17:25:05 -0800

    clock takes the device's current time and displays it

--- assets/clock.js
+++ assets/clock.js
@@ -1,4 +1,4 @@
-funciton initLocalClocks(){
+function initLocalClocks(){
     let date = new Date;
     let seconds = date.getSeconds();
     let minutes = date.getMinutes();
@@ -20,7 +20,18 @@ funciton initLocalClocks(){
         }
     ];
 
-    for (let j= 0; j < hands.length; j++){
+    // Loop through each hand and set the angle
+    for (let j = 0; j < hands.length; j++) {
         let elements = document.querySelectorAll("." + hands[j].hand);
+        for (let k = 0; k < elements.length; k++) {
+            elements[k].style.webkitTransform = 'rotateZ(' + hands[j].angle + 'deg';
+            elements[k].style.transform = 'rotateZ(' + hands[j].angle + 'deg)';
+
+            if (hands[j].hand === 'minutes') {
+                elements[k].parentNode.setAttribute('data-second-angle', hands[j + 1].angle);
+            }
+        }
     }
 }
+
+initLocalClocks()
