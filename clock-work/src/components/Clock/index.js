a4a15f75791b6659a356321c0a06486cf878b471
Connor Lee <45024235+connorleee@users.noreply.github.com>
2019-12-24 15:54:31 -0800

    added js logic to the clock component

--- clock-work/src/components/Clock/index.js
+++ clock-work/src/components/Clock/index.js
@@ -1,7 +1,93 @@
 import React, { Component } from 'react';
 
 class Clock extends Component {
+    
+    componentDidMount = () => {
+        this.initLocalClocks();
+        this.setUpMinuteHands();
+        this.moveSecondHands();
+    }
 
+    initLocalClocks = () => {
+        let date = new Date;
+        let seconds = date.getSeconds();
+        let minutes = date.getMinutes();
+        let hours = date.getHours();
+
+        // array of objects with each hand and it's angle in degrees
+        let hands = [
+            {
+                hand: 'hours',
+                angle: (hours * 30) + (minutes / 2)
+            },
+            {
+                hand: 'minutes',
+                angle: (minutes * 6)
+            },
+            {
+                hand: 'seconds',
+                angle: (seconds * 6)
+            }
+        ];
+
+        // Loop through each hand and set the angle
+        for (let j = 0; j < hands.length; j++) {
+            let elements = document.querySelectorAll("." + hands[j].hand);
+            console.log(elements)
+            for (let k = 0; k < elements.length; k++) {
+                elements[k].style.webkitTransform = 'rotateZ(' + hands[j].angle + 'deg';
+                elements[k].style.transform = 'rotateZ(' + hands[j].angle + 'deg)';
+
+                if (hands[j].hand === 'minutes') {
+                    elements[k].parentNode.setAttribute('data-second-angle', hands[j + 1].angle);
+                }
+            }
+        }
+    }
+
+    setUpMinuteHands = () => {
+        let containers = document.querySelectorAll('.minutes-container');
+        let secondAngle = containers[0].getAttribute('data-second-angle');
+        if (secondAngle > 0) {
+            let delay = (((360 - secondAngle) / 6) + 0.1) * 1000;
+            setTimeout(function () {
+                this.moveMinuteHands(containers);
+            }, delay);
+        }
+    }
+
+    moveMinuteHands = (containers) => {
+        for (let i = 0; i < containers.length; i++) {
+            containers[i].style.webkitTransform = 'rotateZ(6deg)';
+            containers[i].style.transform = 'rotateZ(6deg)';
+        }
+        setInterval(function () {
+            for (let i = 0; i < containers.length; i++) {
+                if (containers[i].angle === undefined) {
+                    containers[i].angle = 12;
+                } else {
+                    containers[i].angle += 6;
+                }
+                containers[i].style.webkitTransform = 'rotateZ(' + containers[i].angle + 'deg)';
+                containers[i].style.transform = 'rotateZ(' + containers[i].angle + 'deg)';
+            }
+        }, 60000);
+    }
+
+    moveSecondHands = () => {
+        var containers = document.querySelectorAll('.seconds-container');
+        setInterval(function () {
+            for (var i = 0; i < containers.length; i++) {
+                if (containers[i].angle === undefined) {
+                    containers[i].angle = 6;
+                } else {
+                    containers[i].angle += 6;
+                }
+                containers[i].style.webkitTransform = 'rotateZ(' + containers[i].angle + 'deg)';
+                containers[i].style.transform = 'rotateZ(' + containers[i].angle + 'deg)';
+            }
+        }, 1000);
+    }
 
     render() {
         return (
